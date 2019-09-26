9dc5a3e48c7e92bba4423c425d782d082b95eede
Maikel Bosschaert <maikel.bosschaert@uhasselt.be>
2019-09-26 15:21:55 +0200

    Added docs folder

---
+++ docs/auto/references.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Lani-Wayda2016"
+    "MR2019242"
+    "Walther2004"
+    "Walter2019"
+    "Walter2019preprint"))
+ :bibtex)
+
